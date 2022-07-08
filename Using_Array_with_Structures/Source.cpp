7d9f199822681f48a19a0bfa6573e968d942c199
DrNyktersten <kurmanbekb001@gmail.com>
2022-07-07 23:57:10 -0500

    array with struct

---
+++ Using_Array_with_Structures/Source.cpp
@@ -0,0 +1,20 @@
+//This program uses an array of structures. 
+#include <iostream>
+#include <iomanip>
+using namespace std; 
+
+struct PayInfo
+{
+	int hours;		//Hours worked 
+	double payRate; //Hourly pay rate
+};
+
+int main()
+{
+	const int NUM_WORKERS = 3;		//Number of workers 
+	PayInfo workers[NUM_WORKERS];	//Array of structures 
+	int index;						//Loop counter 
+
+
+	return 0; 
+}
